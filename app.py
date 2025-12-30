7fbdf371d0961943e28b931435fc411b5f765ff8
Asilbek <Leonlloyd47@gmail.com>
2025-12-30 22:32:12 +0900

    deleting the app.py and adding a new file

--- app.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    def __init__(self, name, age, phone_number):
-        self.name = name
-        self.age = age
-        self.phone_number = phone_number
-
-    def __str__(self):
-        return f'Hello {self.name}, your age is {self.age}, and your phone number is {self.phone_number}'
-
